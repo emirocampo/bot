cd0722e5c4be5643cf0f9b964e03bc3cbcc4a140
emiro campo <emiroluiscampo@gmail.com>
2023-08-03 17:10:51 -0500

    cambio a listas de objetos

--- tester-bot-trader/app.py
+++ tester-bot-trader/app.py
@@ -2,6 +2,9 @@ import csv
 import time
 
 def getListCandles():
+    '''
+    se debe tener el cuidado al pasar a producción el codigo puesto que en caliente el campo DATE y TIME son uno solo
+    '''
     array = []
     ruta = "D:/Documents/python-metatrader5/tester-bot-trader/prueba-datos.csv"
     ruta_2 = "EURUSD1.csv"
@@ -14,9 +17,19 @@ def getListCandles():
     with open(ruta_8,"r") as file:
         reader = csv.reader(file)
         for e in reader:
-            array.append(e)
+            aux = {
+                "DATE":e[0],
+                "TIME":e[1],
+                "OPEN":e[2],
+                "HIGH":e[3],
+                "LOW":e[4],
+                "CLOSE":e[5],
+                "TICKVOL":e[6],
+                "VOL":e[7],
+                "SPREAD":e[8]
+            }
+            array.append(aux)
         return array
-    pass
 
 def getCandle(array,i):
     len(array)
@@ -36,18 +49,18 @@ def getSwingHigh(data):
     se deben ajustar las posiciones el pasar de back-testing a producción
     """
     if( len(data)==3 ):
-        if(data[1][3] > data[0][3] and data[1][3] > data[2][3]):
+        if(data[1]["HIGH"] > data[0]["HIGH"] and data[1]["HIGH"] > data[2]["HIGH"]):
             print("SIWNG HIGH!!!")
             print(f"vela 0 {data[0]}")
             print(f"vela 1 {data[1]}")
             print(f"vela 2 {data[2]}")
             return data[1],True,False
-        elif(data[1][3] == data[2][3]):
+        elif(data[1]["HIGH"] == data[2]["HIGH"]):
             return (), False, True
         else:
             return (), False, False
     if( len(data)==4 ):
-        if(data[1][3] > data[0][3] and data[2][3] > data[3][3]):
+        if(data[1]["HIGH"] > data[0]["HIGH"] and data[2]["HIGH"] > data[3]["HIGH"]):
             print("SIWNG HIGH!!!")
             print(f"vela 0 {data[0]}")
             print(f"vela 1 {data[1]}")
@@ -55,12 +68,12 @@ def getSwingHigh(data):
             print(f"vela 3 {data[3]}")
             data.pop(0)
             return data[1],True,False
-        elif(data[2][3] == data[3][3]):
+        elif(data[2]["HIGH"] == data[3]["HIGH"]):
             return (), False, True
         else:
             return (), False, False
     if(len(data) == 5):
-        if(data[1][3] > data[0][3] and data[3][3] > data[4][3]):
+        if(data[1]["HIGH"] > data[0]["HIGH"] and data[3]["HIGH"] > data[4]["HIGH"]):
             print("SIWNG HIGH!!!")
             print(f"vela 0 {data[0]}")
             print(f"vela 1 {data[1]}")
@@ -80,18 +93,18 @@ def getSwingLow(data):
     se deben ajustar las posiciones el pasar de back-testing a producción
     """
     if( len(data)==3 ):
-        if(data[1][4] < data[0][4] and data[1][4] < data[2][4]):
+        if(data[1]["LOW"] < data[0]["LOW"] and data[1]["LOW"] < data[2]["LOW"]):
             print("SIWNG LOW!!!")
             print(f"vela 0 {data[0]}")
             print(f"vela 1 {data[1]}")
             print(f"vela 2 {data[2]}")
             return data[1],True,False
-        elif((data[1][4] == data[2][4]) and (data[1][4] < data[0][4])):
+        elif((data[1]["LOW"] == data[2]["LOW"]) and (data[1]["LOW"] < data[0]["LOW"])):
             return (), False, True
         else:
             return (), False, False
     if( len(data)==4 ):
-        if(data[1][4] < data[0][4] and data[2][4] < data[3][4]):
+        if(data[1]["LOW"] < data[0]["LOW"] and data[2]["LOW"] < data[3]["LOW"]):
             print("SIWNG LOW!!!")
             print(f"vela 0 {data[0]}")
             print(f"vela 1 {data[1]}")
@@ -99,13 +112,13 @@ def getSwingLow(data):
             print(f"vela 3 {data[3]}")
             data.pop(0)
             return data[1],True,False
-        elif(data[2][4] == data[3][4]):
+        elif(data[2]["LOW"] == data[3]["LOW"]):
             return (), False, True
         else:
             data.pop(0)
             return (), False, False
     if( len(data)==5 ):
-        if(data[1][4] < data[0][4] and data[3][4] < data[4][4]):
+        if(data[1]["LOW"] < data[0]["LOW"] and data[3]["LOW"] < data[4]["LOW"]):
             print("SIWNG LOW!!!")
             print(f"vela 0 {data[0]}")
             print(f"vela 1 {data[1]}")
@@ -121,9 +134,8 @@ def getSwingLow(data):
             return (), False, False
 
 def handSetArraySS(marker,candle,array):
-    aux_candle = [marker]
-    aux_candle.extend(candle)
-    array.append(aux_candle)
+    candle["SS"] = marker
+    array.append(candle)
     return array
 
 
@@ -134,7 +146,7 @@ def handSecuenseShSl(array):
     se deben ajustar las posiciones el pasar de back-testing a producción
     """
     lenght = len(array)
-    if(lenght > 1 and array[lenght - 2][0] == "sh" and array[lenght - 1][0] == "sl" ):
+    if(lenght > 1 and array[lenght - 2]["SS"] == "sh" and array[lenght - 1]["SS"] == "sl" ):
         sl=array[lenght - 1]
         sh=array[lenght - 2]
         print(f"Dentro de handSecuenseShSl")
@@ -148,7 +160,7 @@ def handSecuenseSlSh(array):
     se deben ajustar las posiciones el pasar de back-testing a producción
     """
     lenght = len(array)
-    if(lenght > 1  and array[lenght - 2][0] == "sl" and array[lenght - 1][0] == "sh" ):
+    if(lenght > 1  and array[lenght - 2]["SS"] == "sl" and array[lenght - 1]["SS"] == "sh" ):
         sl=array[lenght - 2]
         sh=array[lenght - 1]
         print(f"Dentro de handSecuenseSlSh")
@@ -202,7 +214,7 @@ def run():
     candles_array = []
     array_ss = []
     array_pair=[]
-    first_break=[[False,"0","0","ss"]]
+    first_break=[{"STATE":False,"SH":"0","SL":"0","NEXT":"S_S"}]
     candles_all = getListCandles()
     highest_high = "0"
     lowest_low = "0"
@@ -219,12 +231,10 @@ def run():
             candle_sl, flag_sl, flag_sl_2 = getSwingLow(candles_array)
             if(flag_sh):
                 array_ss = handSetArraySS("sh",candle_sh,array_ss)
-                if candle_sh[3] > highest_high:
-                    highest_high = candle_sh[3]
+                
             if(flag_sl): 
                 array_ss = handSetArraySS("sl",candle_sl,array_ss)
-                if candle_sl[4] < lowest_low:
-                    lowest_low = candle_sh[4]
+                
             (flag_secuense_Sh, sl, sh) = handSecuenseShSl(array_ss)
             if(flag_secuense_Sh):
                 aux = [sh,sl,["high"]]
@@ -243,47 +253,50 @@ def run():
                         array_pair.append(aux)
                 else:
                     array_pair.append(aux)
-            if(not first_break[0][0]):
+            if(not first_break[0]["STATE"]):
                 if len(array_pair) != 0:
                     for e in reversed(array_pair):
                         if e[2][0] == "high":
-                            if candles_array[2][2] < e[0][4] and candles_array[2][5] > e[0][4] :
+                            if candles_array[2]["OPEN"] < e[0]["HIGH"] and candles_array[2]["CLOSE"] > e[0]["HIGH"] :
                                 print("primer rompimiento -- rompió en el high con cuerpo")
-                                print(f" high roto {e[0][4]}")
+                                print(f" high roto {e[0]['HIGH']}")
                                 print("vela que rompió")
                                 print(candles_array[2])
                                 first_break.pop(0)
-                                first_break.append([True,e[0][4],e[1][5],"low"])
+                                #[{"STATE":False,"SH":"0","SL":"0","NEXT":"S_S"}]
+                                # first_break.append([True,e[0][4],e[1][5],"low"])
+                                first_break.append({"STATE":True,"SH":e[0]["HIGH"],"SL":e[1]["LOW"],"NEXT":"low"})
+                                # first_break = [indica un primer rompimiento,guarda el high roto,guarda el low a romper,indica el siguiente rompimiento]
                                 l = len(array_pair)
                                 for i in range(l):
                                     array_pair.pop(0)
-                                # first_break = [indica un primer rompimiento,guarda el high roto,guarda el low a romper,indica el siguiente rompimiento]
                                 break
                         if e[2][0] == "low" :
-                            if candles_array[2][5] < e[0][5] and candles_array[2][2] > e[0][5]:
+                            if candles_array[2]["CLOSE"] < e[0]["LOW"] and candles_array[2]["OPEN"] > e[0]["LOW"]:
                                 print("primer rompimiento -- rompió con cuerpo en el low")
-                                print(f" low roto {e[0][5]}")
+                                print(f" low roto {e[0]['LOW']}")
                                 print("vela que rompió")
                                 print(candles_array[2])
                                 first_break.pop(0)
-                                first_break.append([True,e[1][4],e[0][5],"high"])
+                                # first_break.append([True,e[1][4],e[0][5],"high"])
+                                first_break.append({"STATE":True,"SL":e[0]["LOW"],"SH":e[1]["HIGH"],"NEXT":"high"})
+                                # first_break = [indica un primer rompimiento,guarda el high a romper,guarda el low roto,indica el siguiente rompimiento]
                                 l = len(array_pair)
                                 for i in range(l):
                                     array_pair.pop(0)
-                                # first_break = [indica un primer rompimiento,guarda el high a romper,guarda el low roto,indica el siguiente rompimiento]
                                 break
             else:
                 print("buscando segundo rompimiento")
-                if first_break[0][3] == "low":
-                    if candles_array[2][5] > highest_high:
+                if first_break[0]["NEXT"] == "low":
+                    if False:
                         #en caso de romper en el mas alto (highest_high) mientras esperaba romper en el low
                         #mi nuevo primer rompimiento (first_break) será la tupla (highest_high - sl-siguiente)
                         #y buscar el segundo rompimiento
                         pass
                     else: 
-                        if candles_array[2][5] < first_break[0][2]:
+                        if candles_array[2]["CLOSE"] < first_break[0]["SL"]:
                             print("segundo rompimiento -- rompió con cuerpo en el low")
-                            print(f" low roto {first_break[0][2]}")
+                            print(f" low roto {first_break[0]['SL']}")
                             print("vela que rompió")
                             print(candles_array[2])
                             print("enviar orden 1")
@@ -291,13 +304,13 @@ def run():
                             # result = mt5.order_send(request)
                             pass
                         pass
-                if first_break[0][3] == "high":
-                    if candles_array[2][5] < lowest_low:
+                if first_break[0]["NEXT"] == "high":
+                    if False:
                         pass
                     else:
-                        if candles_array[2][5] > first_break[0][1]:
+                        if candles_array[2]["CLOSE"] > first_break[0]["SH"]:
                             print("segundo rompimiento -- rompió con cuerpo en el high")
-                            print(f" low roto {first_break[0][1]}")
+                            print(f" high roto {first_break[0]['SH']}")
                             print("vela que rompió")
                             print(candles_array[2])
                             print("enviar orden 2")
@@ -318,12 +331,7 @@ def run():
     print("array_ss")
     for e in array_ss:
         print(e)
-    print(f"sh_b {sh_b};  sl_b {sl_b}")
-        
-        
-        
-        
-        
+
 
 if __name__ == "__main__":
     
